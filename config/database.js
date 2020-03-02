1d16961b53349bc7a8ba4004fe02d5576e5e8447
Sarah <40401149+arghmatey@users.noreply.github.com>
2020-03-01 19:14:38 -0800

    Implement OAuth

--- config/database.js
+++ config/database.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
