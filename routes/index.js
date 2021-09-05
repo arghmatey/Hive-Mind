758c8f4832d6f6285fbb5ce846b9784e2c8c6034
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-09-05 11:56:38 -0700

    rename reports to reviews

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,19 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 router.get('/auth/google', passport.authenticate(
   'google',
   { scope: ['profile', 'email'] }
 ));
+
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect: '/reports',
-    failureRedirect: '/reports'
+    successRedirect: '/',
+    failureRedirect: '/'
   }
 ));
+
 router.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
