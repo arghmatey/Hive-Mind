3d7d043050e5ff4cf68440216e66015062edd6af
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-09-06 19:48:01 -0700

    movie controller to show all popular

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,15 +1,30 @@
 const request = require('request');
 const token = process.env.API_KEY;
-const rootURL = 'https://api.themoviedb.org/3/search/movie?api_key=' + token + '&query=';
 
 module.exports = {
+    allPopular,
     movieSearch,
     show
 };
 
+// i loathe this function and am looking for the right solution
+function allPopular(req, res) {
+    request('https://api.themoviedb.org/3/discover/movie?api_key=' + token + '&with_genres=878', function (err, response, body) {
+        const movies = JSON.parse(body);
+        request('https://api.themoviedb.org/3/discover/tv?api_key=' + token + '&with_genres=10765', function (err, response, body) {
+            const tvShows = JSON.parse(body);
+            console.log(tvShows);
+            res.render('index', {user: req.user, popularMovies: movies.results, popularTV: tvShows.results})
+        });
+        
+    });
+}
+
+// api does not utilize more than 20 results per page, so manual pagination is needed
+// i believe request may be depreciated, so i will need to find a different solution (probably one to use async)
 function movieSearch(req, res) {
     const search = req.query.search;
-    const baseURL = rootURL + search + '&page=';
+    const baseURL = 'https://api.tmdb.org/3/search/movie?api_key=' + token + '&query=' + search + '&page=';
 
     let allResults = [];
     let resultIds = [];
@@ -30,7 +45,7 @@ function movieSearch(req, res) {
             if (movies.page <= movies.total_pages) {
                 gatherMovies();
             } else {
-                res.render('movies/index', { search: search, allResults: allResults, user: req.user })
+                res.render('movies/browse', { search: search, allResults: allResults, user: req.user })
             }
             page++;
         })
@@ -41,6 +56,6 @@ function show(req, res) {
     const movieURL = 'https://api.themoviedb.org/3/movie/' + req.params.id + '?api_key=' + token + '&language=en-US';
     request(movieURL, function (err, response, body) {
         const movieData = JSON.parse(body);
-        res.render('movies/show', { movie: movieData, user: req.user });
+        res.render('movies/details', { movie: movieData, user: req.user });
     });
 }
--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const moviesCtrl = require('../controllers/movies');
 
-router.get('/', moviesCtrl.movieSearch);
-router.get('/:id', moviesCtrl.show)
+router.get('/', moviesCtrl.allPopular);
+router.get('/movies/', moviesCtrl.movieSearch);
+router.get('/movies/:id', moviesCtrl.show);
 
 module.exports = router;
