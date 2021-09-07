eccf1334c6788f7feb2be7ab6e4b49fc8ef4b49e
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-09-06 19:45:55 -0700

    update views with new index page and details

--- routes/comments.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const commentsCtrl = require('../controllers/comments');
-
-router.post('/reports/:id/comments', commentsCtrl.create);
-router.delete('/:rid/comments/:cid', commentsCtrl.delete);
-
-module.exports = router;
